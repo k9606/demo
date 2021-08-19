04f14c04a9fa0fb2919e28bccc8c831971e96718
bishengfei@126.com <19940611hqr>
2021-08-19 15:16:06 +0800

    demo 增删查改

--- routes/api.go
+++ routes/api.go
@@ -5,14 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// RegisterApiRoutes 注册网页相关路由
 func RegisterApiRoutes(r *gin.Engine) {
 	demo := r.Group("/demo")
 	{
-		demo.POST("/create", (&controllers.DemoController{}).Create)
-		demo.POST("/delete", (&controllers.DemoController{}).Delete)
-		demo.POST("/read", (&controllers.DemoController{}).Read)
-		demo.POST("/update", (&controllers.DemoController{}).Update)
-		demo.POST("/list", (&controllers.DemoController{}).List)
+		dc := new(controllers.DemoController)
+		demo.POST("/create", dc.Read)
+		demo.POST("/delete", dc.Delete)
+		demo.POST("/read", dc.Read)
+		demo.POST("/update", dc.Update)
+		demo.POST("/list", dc.List)
 	}
 }
