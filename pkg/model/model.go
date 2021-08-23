cfaf577cb239cf8de02a588b3d1f351985b7395d
bishengfei@126.com <19940611hqr>
2021-08-23 11:02:02 +0800

    配置文件

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"demo/pkg/config"
 	"demo/pkg/logger"
 	"fmt"
 	"gorm.io/driver/mysql"
@@ -18,12 +19,12 @@ func ConnectDB() *gorm.DB {
 
 	// 初始化 MySQL 连接信息
 	var (
-		host     = "127.0.0.1"
-		port     = "3306"
-		database = "demo"
-		username = "root"
-		password = "qqqqqq"
-		charset  = "utf8mb4"
+		host     = config.GetString("database.mysql.host")
+		port     = config.GetString("database.mysql.port")
+		database = config.GetString("database.mysql.database")
+		username = config.GetString("database.mysql.username")
+		password = config.GetString("database.mysql.password")
+		charset  = config.GetString("database.mysql.charset")
 	)
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%t&loc=%s",
 		username, password, host, port, database, charset, true, "Local")
@@ -32,19 +33,18 @@ func ConnectDB() *gorm.DB {
 		DSN: dsn,
 	})
 
-	//var level gormlogger.LogLevel
-	//if config.GetBool("app.debug") {
-	//	// 读取不到数据也会显示
-	//	level = gormlogger.Warn
-	//} else {
-	//	// 只有错误才会显示
-	//	level = gormlogger.Error
-	//}
+	var level gormlogger.LogLevel
+	if config.GetBool("app.debug") {
+		// 读取不到数据也会显示
+		level = gormlogger.Warn
+	} else {
+		// 只有错误才会显示
+		level = gormlogger.Error
+	}
 
 	// 准备数据库连接池
 	DB, err = gorm.Open(gormConfig, &gorm.Config{
-		//Logger: gormlogger.Default.LogMode(level),
-		Logger: gormlogger.Default.LogMode(gormlogger.Info),
+		Logger: gormlogger.Default.LogMode(level),
 	})
 
 	logger.LogError(err)
